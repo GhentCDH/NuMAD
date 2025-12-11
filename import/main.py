01d339e567a5e4256587ed7a8ce61444b778a717
Miel Peeters <peetersmiel@gmail.com>
2025-12-11 22:44:32 +0100

    feat(import): experiment with adding Coins from model

--- import/main.py
+++ import/main.py
@@ -1,16 +1,27 @@
+from sqlmodel import SQLModel, Session
+from src.model import Coin, Identifier
 from src.data import get_data
 from src.db import engine
 
 
 def main():
-    print(engine)
+    SQLModel.metadata.create_all(engine)
 
     reader = get_data()
-    for i, row in enumerate(reader):
-        print(row)
+    identifier_cache: dict[str, Identifier] = {}
 
-        if i == 5:
-            break
+    with Session(engine) as session:
+        for _, row in enumerate(reader):
+            coin = Coin(id=int(row["ID"]))
+
+            if name := row.get("Identified by"):
+                if name not in identifier_cache:
+                    identifier_cache[name] = Identifier(name=name)
+                    session.add(identifier_cache[name])
+                coin.identifier = identifier_cache[name]
+
+            session.add(coin)
+            session.commit()
 
 
 if __name__ == "__main__":
--- import/src/model.py
+++ import/src/model.py
@@ -1,3 +1,19 @@
-import sqlmodel
+from sqlmodel import Field, Relationship, SQLModel
 
 
+class Coin(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    identifier_id: int | None = Field(
+        default=None, foreign_key="identifier.id", ondelete="CASCADE"
+    )
+    identifier: "Identifier" = Relationship(back_populates="coins")
+
+
+class Identifier(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+
+    coins: list[Coin] = Relationship(
+        back_populates="identifier",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"},
+    )
