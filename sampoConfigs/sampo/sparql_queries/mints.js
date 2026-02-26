3d419524b0c84153d675976eba1fbf0ab06a6ffc
ceetto <jahid.chetti@ugent.be>
2026-02-26 14:12:17 +0100

    feat: mints config

--- sampoConfigs/sampo/sparql_queries/mints.js
+++ sampoConfigs/sampo/sparql_queries/mints.js
@@ -1,40 +1,32 @@
-export const authorityProperties = `
+export const mintProperties = `
 {
-    ?id rdf:type nmd:Authority .
-    ?id rdfs:label ?name__prefLabel .
+    ?id a nmo:Mint ;
+        rdfs:label ?name__prefLabel .
+    bind(?id as ?name__id)
     bind(?id as ?uri__id)
     bind(?id as ?uri__prefLabel)
-    bind(?id as ?name__id)
-    bind(concat("/authorities/page/", STRAFTER(str(?id), "ruler/")) as ?uri__dataProviderUrl)
-    bind(?uri__dataProviderUrl as ?name__dataProviderUrl)
+    
+    bind(concat("/mints/page/", STRAFTER(str(?id), "mint/")) as ?name__dataProviderUrl)
+    bind(?mint_dataProviderUrl as ?uri__dataProviderUrl)
 }
 union
 {
     ?id owl:sameAs ?nomismaUri__id .
     bind(?nomismaUri__id as ?nomismaUri__prefLabel)
     bind(?nomismaUri__id as ?nomismaUri__dataProviderUrl)
-}
-union
-{
-    ?id nmo:hasStartDate ?yearStart__id .
-    bind(?yearStart__id as ?yearStart__prefLabel)
-}
-union
-{
-    ?id nmo:hasEndDate ?yearEnd__id .
-    bind(?yearEnd__id as ?yearEnd__prefLabel)
 }
 `
 
-export const authorityPropertiesDetail = `
+export const mintPropertiesDetail = `
 {
-    ?id rdf:type nmd:Authority .
-    ?id rdfs:label ?name__prefLabel .
+    ?id a nmo:Mint ;
+        rdfs:label ?name__prefLabel .
+    bind(?id as ?name__id)
     bind(?id as ?uri__id)
     bind(?id as ?uri__prefLabel)
-    bind(?id as ?name__id)
-    bind(concat("/authorities/page/", STRAFTER(str(?id), "ruler/")) as ?uri__dataProviderUrl)
-    bind(?uri__dataProviderUrl as ?name__dataProviderUrl)
+    
+    bind(concat("/mints/page/", STRAFTER(str(?id), "mint/")) as ?name__dataProviderUrl)
+    bind(?mint_dataProviderUrl as ?uri__dataProviderUrl)
 }
 union
 {
@@ -44,33 +36,37 @@ union
 }
 union
 {
-    ?id nmo:hasStartDate ?yearStart__id .
-    bind(?yearStart__id as ?yearStart__prefLabel)
-}
-union
-{
-    ?id nmo:hasEndDate ?yearEnd__id .
-    bind(?yearEnd__id as ?yearEnd__prefLabel)
-}
-union
-{
-    ?coin__id nmo:hasAuthority ?id ;
+    ?coin__id nmo:hasMint ?id ;
               nmo:hasDenomination ?denomination__id ;
               rdfs:label ?coin__label .
     ?denomination__id rdfs:label ?denomination__prefLabel .
-    
     bind(concat(?coin__label, ", ", ?denomination__prefLabel) as ?coin__prefLabel)
-    
     bind(concat("/coins/page/", STRAFTER(str(?coin__id), "coin/")) as ?coin__dataProviderUrl)
-    
     bind(concat("/denominations/page/", STRAFTER(str(?denomination__id), "denomination/")) as ?denomination__dataProviderUrl)
-    
-    optional
+    {
+        ?coin__id nmo:hasAuthority ?authority__id .
+        ?authority__id rdfs:label ?authority__prefLabel .
+        bind(concat("/authorities/page/", STRAFTER(str(?authority__id), "ruler/")) as ?authority__dataProviderUrl)
+    }
+    union
     {
         ?coin__id nmo:hasMaterial ?material__id .
         ?material__id skos:prefLabel ?material__prefLabel .
         bind(concat("/materials/page/", STRAFTER(str(?material__id), "material/")) as ?material__dataProviderUrl)
-    }
+    }       
+}
+`
+
+export const mintPlaces = `
+SELECT DISTINCT ?id ?lat ?long ?markerColor
+(1 as ?instanceCount) # for heatmap
+WHERE {
+    <FILTER>
+    ?id a nmo:Mint ;
+        schema:geo ?geo .
+    ?geo schema:latitude ?lat ;
+         schema:longitude ?long .
+    BIND("Green" AS ?markerColor)
 }
 `
 
